f245e3623a51ea8d94134f182016c872ad09f669
Alex Christian <alexchristianqr@gmail.com>
2023-08-22 15:23:11 -0500

    [updated] variables desde samconfig y plantilla cf renombrada

--- src/people/dto/create-people.dto.ts
+++ src/people/dto/create-people.dto.ts
@@ -1,4 +1,4 @@
 import { PeopleEntity } from "../people.entity"
 
 // Validaciones de registro
-export class CreatePeopleDto extends PeopleEntity {}
+export interface CreatePeopleDto extends PeopleEntity {}
